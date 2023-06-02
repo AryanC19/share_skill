f3a689a9633722fd1f8fa676c33b70e84f8fb21d
Aryan C <aryanchaudhary1900@gmail.com>
2023-06-02 21:22:31 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,19 @@ import 'package:shareskill1/screens/login_page.dart';
 import 'package:shareskill1/screens/customer_page.dart';
 import 'package:shareskill1/screens/profile_page.dart';
 import 'package:shareskill1/screens/home_page.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:provider/provider.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
